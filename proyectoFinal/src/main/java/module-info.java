4119f576f2ab44f9c0de1e58d5b723795ac281d3
tommyBurgos <trburgos@espol.edu.ec>
2022-08-05 10:39:02 -0500

    Se crea la primera pantalla

--- proyectoFinal/src/main/java/module-info.java
+++ proyectoFinal/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module espol.proyectofinal {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens espol.proyectofinal to javafx.fxml;
     exports espol.proyectofinal;
