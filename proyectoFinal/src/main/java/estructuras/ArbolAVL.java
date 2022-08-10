8d2839b280bbe03e9956e9065bcfce0e96721cb2
tommyBurgos <trburgos@espol.edu.ec>
2022-08-10 07:38:30 -0500

    Se crea por consola el funcionamiento de las preguntas

---
+++ proyectoFinal/src/main/java/estructuras/ArbolAVL.java
@@ -0,0 +1,248 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estructuras;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author tommy_Burgos
+ */
+public class ArbolAVL<E extends Comparable<E>>   {
+    private E data;
+    ArbolAVL left, right;
+
+    public ArbolAVL(E data) {
+        this.data=data;
+        left=right=null;
+    }
+    public void rotacionIzquierda(){
+        ArbolAVL n1=new ArbolAVL(this.data);
+        n1.left=this.left;
+        this.data=(E) this.right.data;
+        this.right=this.right.right;
+        this.left=n1;
+    }
+    public void rotacionDerecha(){
+        ArbolAVL n1=new ArbolAVL(this.data);
+        n1.right=this.right;
+        this.data=(E) this.left.data;
+        this.left=this.left.left;
+        this.right=n1;
+    }
+    public boolean add(E data){
+        boolean insertado=false;
+        if(data.compareTo(this.data)<0){
+            if(this.left==null){
+                this.left=new ArbolAVL(data);
+                insertado= true;
+            }
+            
+            else return this.left.add(data);
+        }else if(data.compareTo(this.data)>0){
+            if(this.right==null){
+                this.right=new ArbolAVL(data);
+                insertado= true;
+            }else{
+                insertado= this.right.add(data);
+            }
+        }else{
+            return true;
+        }
+        if(insertado){
+            this.balancear();
+            return true;
+    }return false;}
+    public void balancear(){
+        int fe=this.factorEquilibrio();
+            if(fe==2){
+                int feD=this.right.factorEquilibrio();
+                if(feD==1){
+                    this.rotacionDerecha();
+                }else if(feD==-1){
+                    
+                }
+            }
+            if(fe==2){
+                int feI=this.left.factorEquilibrio();
+                if(feI==1){
+                    this.rotacionIzquierda();
+                }else if(feI==-1){
+                    
+                }
+        }
+    }
+    public int altura(){
+        int a1,ad;
+        if(left==null)a1=0;
+        else a1=left.altura();
+        
+        if(right==null)ad=0;
+        else ad=right.altura();
+        
+        if(a1>ad) return 1+a1;
+        else return 1+ad;
+    }
+    public int factorEquilibrio(){
+        int ai,ad;
+        if(left==null)ai=0;
+        else ai=left.altura();
+        
+        if(right==null)ad=0;
+        else ad=right.altura();
+        
+        return ad-ai;
+    }
+    public ArbolAVL validarRec(String resp, ArbolAVL arb){
+        if(resp.equals("Si") || resp.equals("si")) return arb.left;
+        else if(resp.equals("No")|| resp.equals("no")) return arb.right;
+        System.out.println("Asegurese de estar escribiendo bien la orden");
+        return null;
+    }
+    
+    public boolean addRec(E data, ArbolAVL preg){        
+        if(data.equals("Si")){
+            if(this.left==null){
+                this.left=preg;
+                return true;
+            }
+            
+            else return this.left.addRec(data,preg);
+        }else if(data.equals("No")){
+            if(this.right==null){
+                this.right=preg;
+                return true;
+            }else{
+                return this.right.addRec(data,preg);
+            }
+        }else{
+            return true;
+        }
+    }
+    /*public boolean addRec(LinkedList<String> preguntas, LinkedList<String> recorrido){
+        
+        for (int i = 0; i < recorrido.size(); i++) {
+            
+        }
+    }*/
+    
+    public static LinkedList leerInfo(String nomfile){
+        File f = new File(nomfile);
+        if(f.exists())System.out.println("Existe");
+        else{
+           System.out.println("no Existe");
+        }
+        LinkedList info = new LinkedList<>();
+        try(Scanner sc = new Scanner(new File(nomfile))){
+            System.out.println("hola Perros");
+            while (sc.hasNextLine())
+            {
+                String linea = sc.nextLine();               
+                String[] tokens = linea.split(",");                                
+                info.addFirst(tokens);
+                
+                
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage()+"aaaaa");
+        }
+        return info;
+}
+
+    public E getData() {
+        return data;
+    }
+
+    public ArbolAVL getLeft() {
+        return left;
+    }
+
+    public ArbolAVL getRight() {
+        return right;
+    }
+    
+    
+    public static void main(String[] args) {
+        ArbolAVL raiz=new ArbolAVL(80);
+        ArbolAVL raiz2=new ArbolAVL("Es un mamifero?");
+        
+        //Lado Izquierdo principal
+        String p1="este animal es carnívoro?";
+        ArbolAVL nodo1=new ArbolAVL(p1);
+        raiz2.addRec("Si", nodo1);              
+        //Tercer nivel Si
+        String p3="se para sobre 4 patas?";
+        ArbolAVL nodo3=new ArbolAVL(p3);
+        nodo1.addRec("Si", nodo3);
+        //Tercer nivel No
+        ArbolAVL nodo4=new ArbolAVL(p3);
+        nodo1.addRec("No", nodo4);
+        
+        //Nivel final    
+        String an1="Oso";
+        ArbolAVL nodo5=new ArbolAVL(an1);
+        nodo3.addRec("Si", nodo5);
+        
+        String an2="Venado";
+        ArbolAVL nodo7=new ArbolAVL(an2);
+        nodo4.addRec("Si", nodo7);
+        
+        //Lado Derecho principal
+        ArbolAVL nodo2=new ArbolAVL(p1);
+        raiz2.addRec("No", nodo2);
+
+        
+        raiz.add(100);
+        raiz.add(60);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Escribe tu nombre: ");
+        String nombre=sc.nextLine();
+        System.out.println("*******************"+"BIENVENIDO "+nombre+"*******************");
+        System.out.println("Soy Patronus y voy a intentar adivinar el animal que estas pensando");
+        System.out.println("Piensa en un animal");
+        System.out.println("Listo? (Si/No): ");
+        String continuar=sc.next();
+        ArbolAVL direccion=raiz2;
+        while(continuar.equals("Si") || continuar.equals("si")){
+            System.out.println("Listo!! Vamos a empezar");
+            System.out.println("Te hare unas cuantas preguntas antes de decirte mi respuesta");
+            System.out.println("Si deseas salir solo deberas escribir (SALIR) y volveras ");
+            System.out.println("Deberas responder estas preguntas escribiendo Si o No (Si) para continuar");
+            String resp=sc.next();
+            while((resp!="Salir" || resp!="salir") && (direccion.left!=null || direccion.right!=null)){
+                System.out.println(direccion.data);
+                resp=sc.next();
+                direccion=direccion.validarRec(resp, direccion);
+                
+                               
+            }System.out.println("El animal en el que estas pensando es un "+direccion.data + "?");
+            break;                                                                                               
+            
+        }
+        System.out.println(raiz2.left.right.left.data);
+        System.out.println(raiz2.left.left.left.data);
+        /*boolean f= raiz.add(110);
+        System.out.println(raiz2.data);
+        System.out.println("Si->"+raiz2.left.data);
+        System.out.println("No->"+raiz2.right.data);
+        System.out.println("Si->"+raiz2.left.left.data);
+        System.out.println("Si->"+raiz2.left.left.left.data);
+        
+        
+        System.out.println("-----------");
+        System.out.println(raiz);
+        raiz.rotacionIzquierda();
+        System.out.println(raiz.data);
+        System.out.println(f);
+        */
+    }
+    
+    
+    
+}
--- proyectoFinal/src/main/java/estructuras/ArbolBinario.java
+++ proyectoFinal/src/main/java/estructuras/ArbolBinario.java
@@ -8,149 +8,227 @@ package estructuras;
  *
  * @author tommy_Burgos
  */
-public class ArbolBinario<E extends Comparable<E>> {
-    private E data;
-    private ArbolBinario<E> left,right;
-    
-    public ArbolBinario(E data){
-        this.data=data;
-        left=right=null;
-    }
-    public boolean isEmpty(){
-        if(data==null)return true;
-        //if(left.data==null && right.data==null) return true;                      
-        return false;
-    }
-    public int altura(){
-        if(data==null)return 0;
-        int izq,der;
-        izq=der=0;
+public class ArbolBinario <E extends Comparable<E>>  {
+    //---------------- nested Node class ----------------
+    private static class Node<E> {
+        private E element;// reference to the element stored at this node
+        private Node<E> left;
+        private Node<E> right;
         
-        return 1+ Math.max(left.altura(),right.altura());
-    }
-    public boolean esHoja(){
-        return this.right==null && this.getLeft()==null;
-    }
-    public void recorridoPrefijo(){
-        System.out.println(data);
-        if(left!=null)left.recorridoPrefijo();
-        if(right!=null)right.recorridoPrefijo();
+        public E getElement( ) { return element; }
+        public Node<E> getLeft( ) { return left; }
+        public Node<E> getRigh( ) { return right; }
+        public void setLeft(Node<E> n) { left = n; }
+        public void setRight(Node<E> n) { right = n; }
+        
+        public Node( ) { }
+        public Node( E rootData) { element = rootData;}
+        
+        public boolean esHoja(){
+            return this.getRigh()==null && this.getLeft()==null;
+        }
+        @Override
+        public String toString(){
+            return element.toString();
+        }
+        
+       
+    } //----------- end of nested Node class ---------
+    
+    // instance variables of BinaryTree
+    private Node<E> root = null;// root node of the tree (or null if empty)
+        
+    public ArbolBinario( ) { }// constructs an initially empty tree
+    
+    public ArbolBinario(E rootData) {
+        root = new Node(rootData);
     }
     
-    public void recorridoPosfijo(){
-        System.out.println(data);
-        if(left!=null)left.recorridoPrefijo();
-        if(right!=null)right.recorridoPrefijo();
+    
+    public String preOrden(){
+        return preOrden(root).replaceAll(" +", " ").substring(1);  
+    }
+    private String preOrden(Node<E> p){
+        if (p == null) return " ";
+        return " " + p.toString() + " " + preOrden(p.left) +" "+ preOrden(p.right);
     }
     
-    public void recorridoInfijo(){
-        System.out.println(data);
-        if(left!=null)left.recorridoPrefijo();
-        if(right!=null)right.recorridoPrefijo();
+    private boolean isEmpty(){
+        return root==null;
     }
-    public boolean insert(E element){
-        ArbolBinario root=new ArbolBinario(data);
-        if(element==null)return false;
-        root = insert(root,element);
+    public String inOrden(){
+        if (root == null) return "";
+        return inOrden(root).replaceAll(" +", " ").substring(1);
+    }
+    private String inOrden(Node<E> p){
+        if(p == null) return "";
+        return " " + inOrden(p.left) +" "+ p.toString() + " " +  inOrden(p.right);
+    }
+    
+    
+    public String postOrden(){
+        if (root == null) return "";
+        return postOrden(root).replaceAll(" +", " ").substring(1);  
+    }
+    private String postOrden(Node<E> p){
+        if(p == null) return "";
+        return " " + postOrden(p.left) +" "+ postOrden(p.right)+ " " +p.toString() ;
+    }
+    
+    
+    public boolean contains(E element){
+        if (element == null )
+            return false;
+        return contains(root, element);
+    }
+    private boolean contains(Node<E> p, E element){
+        if (p == null)
+            return false;
+        if(element.compareTo(p.element) == 0){
+            return true;
+        }
+        if(element.compareTo(p.element) > 0)
+            return contains(p.right, element);
+        return contains(p.left, element);
+    }
+    
+    
+    public boolean add(E element){
+        if (element == null)
+            return false;
+        root = add(root, element);
         return true;
     }
-    
-    public ArbolBinario insert(ArbolBinario ab, E dato){
-      
-        if(ab==null)return new ArbolBinario<>(dato);
-        if(dato.compareTo((E)ab.data)==0)return ab;
-        if(dato.compareTo((E)ab.data)>0){
-            ab.right=insert(ab.right,dato);
-            System.out.println(ab.right.data);
-        }
-        else{
-            ab.left=insert(ab.left,dato);
-        }return ab;
+    private Node<E> add(Node<E> p, E element){
+        if(p == null)
+             return new Node(element);
+        if (element.compareTo(p.element)== 0)
+             return p;
+        if (element.compareTo(p.element) > 0)
+             p.right= add(p.right, element);
+        else
+             p.left= add(p.left, element);
+        return p;
+    }
+
+    public Node<E> getRoot() {
+        return root;
     }
     
-    public ArbolBinario search(ArbolBinario ab, E dato){
-        if(this.isEmpty())return null;
-        ArbolBinario arbIzq=this.left;
-        ArbolBinario arbDer=this.right;
-        if(this.esHoja()){    
-            if(arbIzq.data.compareTo(dato)==0) return arbIzq;
-            else if(arbDer.data.compareTo(dato)==0) return arbDer;
-        }search(arbIzq, dato);
-        search(arbDer, dato);
+    
+    public int altura(Node arbol){                
+        if(arbol==null){
+            return 0;
+        }
+        if(arbol!=null) 
+            return 1 + Math.max(altura(arbol.getLeft()), altura(arbol.getRigh())) ;       
+        return 0;
+    }
+    //Contar la cantidad de nodos que existen el arbol
+    public int size(Node arb){
+        //Caso Base
+        if(arb==null){//Si el arbol se encuentra vacio, retornara que tiene 0
+            return 0;
+        }
+        //Recursivo
+        if(arb!=null){
+            /*Se realizará el conteo por cada lado de los nodos
+            de forma recursiva, para así repetir el proceso hasta el final, para luego sumar el total de cada lado*/
+            int contIzq= size(arb.getLeft());
+            int contDer= size(arb.getRigh());
+            int total=contIzq+contDer;
+            return total;
+        }
+        return 0;
+    }
+  //Para contar cuantas hojas existen en el arbol
+    public int hojas(Node arb){
+        int var=0;
+        //Caso Base
+        //Si se define que el nodo ingresado es hoja, retoran 1
+        if(arb.esHoja()){
+            return 1;
+        }else{//Caso Recursivo         
+            if(arb.getLeft()!=null){
+                var +=hojas(arb.getLeft());
+            }
+            if(arb.getRigh()!=null){
+                var+=hojas(arb.getRigh());
+            }
+        }
+        return var;
+    }
+    //Cuenta la cantidad de nodos alojados en lado izquierdo del arbol
+    public int izquierdos(Node arb){
+        //Caso Base
+        if(arb == null){
+            return 0;
+        }else{//Caso Recursivo
+            izquierdos(arb.getLeft());
+            if(arb != root){
+                System.out.println(arb.element+"-");
+            }
+        }
+        return 0;
+    }
+    
+    //TAREA #10
+    public boolean esEspejo (ArbolBinario<E> arbol2){
+        ArbolBinario<E> ayuda1=null;
+        ArbolBinario<E> ayuda2=null;
+        if(arbol2==null)return false;
+        if(this.root!= arbol2.root)return false;
+        boolean bd1=false;
+        boolean bd2=false;
+        if(arbol2!=null){
+            if(arbol2.root.getLeft()==this.root.right){
+                ayuda1.root=arbol2.root.getLeft();                
+                bd1=true;
+                ayuda1.esEspejo(arbol2);                
+            }
+            if(arbol2.root.getRigh()==this.root.left){
+                ayuda2.root=arbol2.root.getLeft();
+                bd2=true;
+                ayuda2.esEspejo(arbol2);
+            }
+        }
+        return(bd1 && bd2);        
+    }
+    /*
+    public boolean esEspejo (ABB<E> arbol2){
         
-        return null;
+        Node<E> ext=root;
+        Node<E> nuevo=arbol2.getRoot();
+        if(arbol2==null) return false;
+         if(arbol2!=null){            
+            if(arbol2.root.left.getElement()==ext.right.getElement()){            
+                ext=ext.right;
+                nuevo=arbol2.getRoot().left;
+            }if(nuevo.right.getElement()==ext.left.getElement()){            
+                ext=ext.left;
+                nuevo=arbol2.getRoot().right;            
+        }arbol2.esEspejo(arbol2);
+         }      
+        return false;        
     }
-    public ArbolBinario encontrarMenorDLMayores(ArbolBinario ab){
-        if(ab.left==null) return null;
-        ArbolBinario mEm= ab;
-        while(mEm.right !=null){
-            mEm=mEm.right;
-    }return mEm;
-    }
-    
-    public ArbolBinario delete(ArbolBinario ab, E dato){
-        ArbolBinario mEm=encontrarMenorDLMayores(ab);
-       if(ab.isEmpty())return null;
-       if(ab.data.compareTo(dato)== 0){
-            if(ab.esHoja()) return null;
-            if(ab.right == null)  return ab.left;
-            if(ab.left  == null)  return ab.right;
-            ab.data=(encontrarMenorDLMayores(ab.left).data);
-            ab.left= delete(ab.left, (E)ab.data);
-            return ab;
-        }if(ab.data.compareTo(dato)>0){
-            ab.right= delete(ab.right, dato);
-        }else{
-            ab.left= delete(ab.left, data);
+    */
+      public Node<E> espejo (Node<E> p){        
+        ArbolBinario<E> ext =null;        
+        ext.root=p;
+        Node<E> ingDe=p.right;
+        Node<E> ingIzq=p.left;
+        if(p==null){
+            return ext.root;
         }
-        return ab;
+        if(p!=null){
+            ext.root.left=ingDe;          
+            ext.root.right=ingIzq;
+            
+            ext.espejo(ext.root.left);
+            ext.espejo(ext.root.right);
+        }        
+        return ext.root;
+        
     }
-    
- @Override
-    public String toString() {
-        return "ArbolBinario{" + "data=" + data + ", left=" + left.data + ", right=" + right.data + '}';
-    }
-    public E getData() {
-        return data;
-    }
-
-    public ArbolBinario<E> getLeft() {
-        return left;
-    }
-
-    public ArbolBinario<E> getRight() {
-        return right;
-    }
-
-    public void setData(E data) {
-        this.data = data;
-    }
-
-    public void setLeft(ArbolBinario<E> left) {
-        this.left = left;
-    }
-
-    public void setRight(ArbolBinario<E> right) {
-        this.right = right;
-    }
-
-   public static void main(String[] args) {
-         ArbolBinario<Integer> raiz=new ArbolBinario<Integer>(3);
-         raiz.insert(4);
-         //raiz.insert(5);
-         //raiz.insert(2);
-         
-         raiz.setLeft(new ArbolBinario<Integer>(37));
-         System.out.println(raiz);
-         raiz.right=new ArbolBinario<Integer>(49);
-         System.out.println(raiz); 
-         
-         System.out.println(raiz.data);
-         System.out.println(raiz.left.data);
-         System.out.println(raiz.right.data);
-         System.out.println(raiz.isEmpty());
-}
-    
-    
-    
+       
 }
